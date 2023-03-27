01a642ce5d268cca5981b1ab224e0afdfe6f900d
Nathália <nathaliamassis@gmail.com>
2023-03-27 11:50:38 -0300

    fix

--- prisma/migrations/20230327143303_/migration.sql
+++ prisma/migrations/20230327143303_/migration.sql
@@ -14,6 +14,8 @@ CREATE TABLE "check_ins" (
     "id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "validated_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+    "gym_id" TEXT NOT NULL,
 
     CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
 );
@@ -24,7 +26,7 @@ CREATE TABLE "gyms" (
     "title" TEXT NOT NULL,
     "description" TEXT,
     "phone" TEXT,
-    "latitue" DECIMAL(65,30) NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
     "longitude" DECIMAL(65,30) NOT NULL,
 
     CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
@@ -32,3 +34,9 @@ CREATE TABLE "gyms" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_gym_id_fkey" FOREIGN KEY ("gym_id") REFERENCES "gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
